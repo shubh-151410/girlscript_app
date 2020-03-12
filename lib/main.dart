6ef088bb2165441fc4a2ed337f70d853803c988c
Shubhanshu <shubhanshu814@gmail.com>
2020-03-12 13:29:53 +0530

    second commit removing debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:noob_project/app.dart';
 
-main(){
-  return runApp(new MaterialApp(
-    home: new MyApp(),
-  ));
+main() {
+  return runApp(
+    new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new MyApp(),
+    ),
+  );
 }
